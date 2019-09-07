817bd9551e588fdbd1b171b854983c546b7ad81e
Tomasz Figa <tomasz.figa@gmail.com>
2019-09-07 19:49:10 +0900

    HexUtils: Document the code

--- src/HexUtils.js
+++ src/HexUtils.js
@@ -1,10 +1,30 @@
-export const BOARD_WIDTH = 5;
-export const BOARD_HEIGHT = 9;
-export const MAX_PLAYERS = 4;
-export const CACHE_SIZE = 3;
-export const CELLS_SIZE = MAX_PLAYERS * CACHE_SIZE
-                          + BOARD_WIDTH * BOARD_HEIGHT;
+/**
+ * @fileoverview hex board is projected into a grid of 5x9 cells
+ * to allow easy addressing and help rendering in the client.
+ * Moreover, the game cells array is prepended with cells that
+ * correspond to player's caches.
+ * This file contains utilities to deal with this model.
+ */
 
+/** Width of the cell grid used by the hex board. */
+export const BOARD_WIDTH = 5;
+/** Height of the cell grid used by the hex board. */
+export const BOARD_HEIGHT = 9;
+/** Maximum number of players in the game. */
+export const MAX_PLAYERS = 4;
+/** Number of token slots in the cache. */
+export const CACHE_SIZE = 3;
+/** Number of cells used for the caches. */
+export const CACHE_CELLS = MAX_PLAYERS * CACHE_SIZE;
+/** Total size of the game cells array. */
+export const CELLS_SIZE = CACHE_CELLS + BOARD_WIDTH * BOARD_HEIGHT;
+
+/**
+ * Checks whether given board grid coordinates correspond to a hex.
+ * @param {number} x The horizontal coordinate.
+ * @param {number} y The vertical cordinate.
+ * @return {boolean} true if given coordinates correspond to a hex.
+ */
 export function XyIsValid(x, y) {
   // 2
   // 1 3
@@ -30,10 +50,21 @@ export function XyIsValid(x, y) {
   }
 }
 
+/**
+ * Translates hex grid coordinates into game cells array index.
+ * @param {number} x Horizontal coordinate.
+ * @param {number} y Vertical coordinate.
+ * @return {number} Index in the game cells array.
+ */
 export function XyToPos(x, y) {
   return MAX_PLAYERS * CACHE_SIZE + y * BOARD_WIDTH + x;
 }
 
+/**
+ * Translates game cells array index into corresponding player ID.
+ * @param {number} pos Game cells array index.
+ * @return {number} Player ID.
+ */
 export function CachePosToPlayer(pos) {
   const player = Math.floor(pos / CACHE_SIZE);
   if (player >= MAX_PLAYERS)
@@ -41,10 +72,21 @@ export function CachePosToPlayer(pos) {
   return player;
 }
 
+/**
+ * Checks whether given cells array index is a cache.
+ * @param {number} pos Game cells array index.
+ * @return {boolean} True if the index is a cache.
+ */
 export function PosIsCache(pos) {
   return pos < CACHE_SIZE * MAX_PLAYERS;
 }
 
+/**
+ * Returns a game cells array index for given player's cache index.
+ * @param {number} player Player ID.
+ * @param {number} pos Index in the cache.
+ * @return {number} Index to the game cells array.
+ */
 export function PlayerCachePos(player, pos) {
   return player * CACHE_SIZE + pos;
 }
