4513b742a0b0ac3c71fd9b87f2fd39bc6924e20f
Tomasz Figa <tomasz.figa@gmail.com>
2019-09-13 22:53:44 +0900

    HexBoard: Add drawing of toughness used marks

--- src/TokenHex.tsx
+++ src/TokenHex.tsx
@@ -2,6 +2,44 @@ import React from 'react';
 import * as HexBoardUtils from './HexBoardUtils';
 import { Hex } from './Hex';
 
+interface FloatingContainerProps {
+  /* Bottom position in pixels. */
+  bottom?: number;
+  /* Height in pixels. */
+  height?: number;
+  /* Left position in pixels. */
+  left?: number;
+  /* Right position in pixels. */
+  right?: number;
+  /* Top position in pixels. */
+  top?: number;
+  /* Width in pixels. */
+  width?: number;
+}
+
+/**
+ * A simple container for anything that needs absolute positioning within
+ * the token area.
+ */
+class FloatingContainer extends React.Component<FloatingContainerProps, {}> {
+  render() {
+    const floatingStyle: React.CSSProperties = {
+      bottom: this.props.bottom && this.props.bottom + 'px',
+      height: this.props.height && this.props.height + 'px',
+      left: this.props.left && this.props.left + 'px',
+      position: 'absolute',
+      right: this.props.right && this.props.right + 'px',
+      top: this.props.top && this.props.top + 'px',
+      width: this.props.width && this.props.width + 'px',
+    };
+    return (
+      <div style={floatingStyle}>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
 interface TokenHexProps {
   /** Whether the hex is currently selected. */
   active: boolean;
@@ -79,11 +117,32 @@ export class TokenHex extends React.Component<TokenHexProps, {}> {
       width: '100%',
       zIndex: 15,
     };
+    const toughnessUsedMarks = [];
+    const toughnessUsedImage=require('./resources/ability_toughness_used.png');
+    const toughnessUsedStyle: React.CSSProperties = {
+      backgroundImage: `url(${toughnessUsedImage})`,
+      backgroundPosition: 'center center',
+      backgroundSize: 'contain',
+      width: '100%',
+      height: '100%',
+    };
+    for (let i = 0; i < hex.damage; ++i) {
+      let left = 27 + i * 30;
+      if (hex.damage > 2)
+        left = 25 + 45 * i / hex.damage;
+      toughnessUsedMarks.push(
+        <FloatingContainer width={34} height={34} bottom={7} left={left} key={i}>
+          <div style={toughnessUsedStyle}>
+          </div>
+        </FloatingContainer>
+      );
+    }
     return (
       <>
         <div style={fullHexStyle} ref={this.tokenNode}
              onClick={(e) => this.props.onClick(e, this.props.pos)}
              onWheel={(e) => this.props.onWheel(e, this.props.pos)}>
+          {toughnessUsedMarks}
         </div>
         <div style={overlayStyle} ref={this.overlayNode}
              onClick={(e) => this.props.onClick(e, this.props.pos)}
